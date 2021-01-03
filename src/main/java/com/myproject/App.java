31d59a5d250a6d0639cc2fbf8183ffb9cbfc92aa
John Doe <@noreply>
2021-01-03 20:40:59 +0100

    Test different author

--- src/main/java/com/myproject/App.java
+++ src/main/java/com/myproject/App.java
@@ -14,7 +14,6 @@ import static java.time.ZoneOffset.UTC;
 
 @Slf4j
 public class App {
-
     public static void main(String[] args) {
         var price = getPriceHistory("LMT")
                 .extract().as(PriceChartV8Dto.class, GSON);
