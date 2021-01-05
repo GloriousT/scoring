2eea4527619863225220661be3365d6cf46b1b38
GloriousT <@noreply>
2021-01-05 16:24:03 +0100

    Fetch macrotrends company name on a fly

--- src/main/java/com/myproject/client/MacroTrendsClient.java
+++ src/main/java/com/myproject/client/MacroTrendsClient.java
@@ -2,19 +2,37 @@ package com.myproject.client;
 
 import io.restassured.response.ValidatableResponse;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import static io.restassured.RestAssured.given;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @ToString
 public class MacroTrendsClient {
     private final String ticker;
+    private String companyPathName;
 
     public ValidatableResponse getEarningsHistory() {
+        if (null == companyPathName) {
+            fetchCompanyPathName();
+        }
         return given()
                 .log().uri().log().method()
-                .get("https://www.macrotrends.net/stocks/charts/LMT/lockheed-martin/eps-earnings-per-share-diluted")
+                .get(String.format("https://www.macrotrends.net/stocks/charts/%s/%s/eps-earnings-per-share-diluted",
+                        ticker,
+                        companyPathName))
                 .then().log().ifValidationFails();
+    }
+
+    private void fetchCompanyPathName() {
+        var location = given().log().method().log().uri()
+                .redirects().follow(false)
+                .get(String.format("https://www.macrotrends.net/stocks/charts/%s", ticker))
+                .then().log().ifValidationFails()
+                .statusCode(301)
+                .extract().header("Location")
+                .split("/");
+        companyPathName = location[location.length - 1];
     }
 }
